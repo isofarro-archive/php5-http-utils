4317033e66081eb78eb81e4002b5407d4dba33ff
Isofarro <isofarro2@gmail.com>
2009-05-05 21:20:16 +0100

    HttpRequest URL manipulation + tests

--- HttpRequest.php
+++ HttpRequest.php
@@ -2,8 +2,8 @@
 
 class HttpRequest {
 	protected $method  = 'GET'; 
-	protected $path;	   // Path segment, without query string (mandatory)
-	protected $query;    // Query string (optional)
+	//protected $path;	   // Path segment, without query string (mandatory)
+	//protected $query;    // Query string (optional)
 	protected $version = 'HTTP/1.1';
 	
 	protected $headers = array();
@@ -11,21 +11,69 @@ class HttpRequest {
 	
 	// Raw data
 	protected $url;
-	protected $urlParts;
+
 
 	public function __construct($url = false) {
+		$this->url = new HttpUrl();
 		if ($url) {
 			$this->setUrl($url);
 		}
 	}
 	
 	public function setUrl($url) {
-		$this->url = new HttpUrl($url);
+		$this->url->setUrl($url);
 	}
 	
 	public function getUrl() {
 		return $this->url->getUrl();
 	}
+	
+	public function setRelativeUrl($url) {
+		$this->url->setRelativeUrl($url);
+	}
+	
+	public function getPath() {
+		if ($this->url) {
+			return $this->url->getPath();
+		}
+	}
+	
+	public function setPath($path) {
+		if ($this->url) {
+			$this->url->setPath($path);
+		}
+	}
+
+	public function getQuery() {
+		if ($this->url) {
+			return $this->url->getQuery();
+		}
+	}
+	
+	public function getQueryString() {
+		if ($this->url) {
+			return $this->url->getQueryString();
+		}
+	}
+	
+	public function setQuery($query) {
+		if ($this->url) {
+			$this->url->setQuery($query);
+		}
+	}
+
+	public function getHost() {
+		if ($this->url) {
+			return $this->url->getHost();
+		}
+	}
+	
+	public function setHost($host) {
+		if ($this->url) {
+			$this->url->setHost($host);
+		}
+	}
+
 
 }
 
--- tests/HttpRequestTests.php
+++ tests/HttpRequestTests.php
@@ -26,6 +26,52 @@ class HttpRequestTests extends PHPUnit_Framework_TestCase {
 		$this->request->setUrl($url);
 		
 		$this->assertNotNull($this->request->getUrl());
+		$this->assertEquals($url, $this->request->getUrl());
+		$this->assertEquals('example.org', $this->request->getHost());
+		$this->assertEquals('/path/file.php',   $this->request->getPath());
+		
+		$query = $this->request->getQuery();
+		$this->assertType('array', $query);
+		$this->assertArrayHasKey('key1', $query);
+		$this->assertEquals('val1', $query['key1']);
+		$this->assertArrayHasKey('key2', $query);
+		$this->assertEquals('val2', $query['key2']);
+
+		$this->assertEquals('key1=val1&key2=val2', $this->request->getQueryString());
+	}
+
+	public function testSetHost() {
+		$url      = 'http://example.com/file.php';
+		$expected = 'http://example.org/file.php';
+		$this->request->setUrl($url);
+
+		$this->assertEquals($url, $this->request->getUrl());
+		$this->request->setHost('example.org');
+		$this->assertEquals('example.org', $this->request->getHost());
+		$this->assertEquals($expected, $this->request->getUrl());
+		
+	}
+
+	public function testChangePath() {
+		$url      = 'http://example.org:8080/path/file.php';
+		$expected = 'http://example.org:8080/newfile.php';
+
+		$this->request->setUrl($url);
+		$this->assertEquals($url, $this->request->getUrl());
+
+		$this->request->setUrl('/newfile.php');	
+		$this->assertEquals($expected, $this->request->getUrl());
+	}
+
+	public function testRelativeChangePath() {
+		$url      = 'http://example.org:8080/path/file.php';
+		$expected = 'http://example.org:8080/path/newfile.php';
+
+		$this->request->setUrl($url);
+		$this->assertEquals($url, $this->request->getUrl());
+
+		$this->request->setRelativeUrl('newfile.php');	
+		$this->assertEquals($expected, $this->request->getUrl());
 	}
 
 }
