7c319b6debffd4377b88c2199f728768f0dd105a
Isofarro <isofarro2@gmail.com>
2009-06-15 08:13:44 +0100

    skeleton of CanonicalLink class

--- CanonicalLink.php
+++ CanonicalLink.php
@@ -1,11 +1,27 @@
 <?php
 
 class CanonicalLink {
+	var $config;
 	var $cache;
 	var $http;
 
+	public function __construct($config=false) {
+		if ($config) {
+			$this->setConfig($config);
+		}
+	}
+	
+	public function setConfig($config) {
+		$this->config = $config;
+		$this->_init();
+	}
+
 	public function getCanonicalLink($url) {
 		return $url;
+	}
+	
+	protected function _init() {
+	
 	}
 }
 
