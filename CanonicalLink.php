82459ac16b058e0c2425d934d91b89e506746f07
Isofarro <isofarro2@gmail.com>
2009-06-16 21:03:21 +0100

    Bare-bones implementation of spotting and following an HTTP redirect

--- CanonicalLink.php
+++ CanonicalLink.php
@@ -8,6 +8,8 @@ class CanonicalLink {
 	var $config;
 	var $cache;
 	var $http;
+	
+	var $lookup = array();
 
 	public function __construct($config=false) {
 		if ($config) {
@@ -21,8 +23,49 @@ class CanonicalLink {
 	}
 
 	public function getCanonicalLink($url) {
-		return $url;
+		if (!empty($this->lookup[$url])) {
+			echo '!';
+			return $this->lookup[$url];
+		}
+		
+		$newUrl   = $url;
+		$response = $this->_getUrl($url);
+		$status = $response->getStatus();
+		
+		if ($status==301 || $status==302) {
+			// Redirection taking place
+			//echo "Response: "; print_r($response);
+			$location = $response->getHeader('Location');
+			//echo "Response redirecting to: {$location}\n";
+			
+			// TODO: Keep following the redirection until we arrive at
+			//			a non-redirecting page
+			
+			$newUrl = $location;
+		} else {
+			// No redirection
+			// TODO: Check for iframe Digg-toolbar like markup
+		}
+		
+		// Cache any canonical references found
+		if ($newUrl !== $url) {
+			$this->lookup[$url] = $newUrl;
+		}
+		
+		// Default: return starting url
+		return $newUrl;
 	}
+	
+
+	protected function _getUrl($url) {
+		if (!$this->http) {
+			$this->http = new HttpClient();
+		}
+		
+		$request = new HttpRequest($url);
+		$response = $this->http->doRequest($request);
+		return $response;
+	}	
 	
 	protected function _init() {
 	
---
+++ tests/CanonicalLinkTest.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once dirname(dirname(__file__)) . '/CanonicalLink.php';
+require_once dirname(dirname(__file__)) . '/HttpClient.php';
+require_once dirname(dirname(__file__)) . '/HttpCache.php';
+require_once dirname(dirname(__file__)) . '/HttpRequest.php';
+require_once dirname(dirname(__file__)) . '/HttpResponse.php';
+require_once dirname(dirname(__file__)) . '/HttpUtils.php';
+
+class CanonicalLinkTest extends PHPUnit_Framework_TestCase {
+	var $canon;
+	
+	public function setUp() {
+		$this->canon = new CanonicalLink();
+	}
+
+	public function testNormalUrl() {
+		$url = 'http://www.isolani.co.uk/';
+		$canonUrl = $this->canon->getCanonicalLink($url);
+		$this->assertEquals($url, $canonUrl);
+	}
+	
+	public function testTinyUrl() {
+		$url = 'http://tinyurl.com/qyx9uu';
+		$endUrl = 'http://www.isolani.co.uk/blog/web/YahooOpenHackLondon2009';
+		$canonUrl = $this->canon->getCanonicalLink($url);
+		$this->assertEquals($endUrl, $canonUrl);
+	}
+
+}
+
+
+?>
